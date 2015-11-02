eee7d8543bc0680cda22a7a0d5d1a6c045083642
Chris Keathley <c@keathley.io>
2015-11-02 17:32:51 -0500

    Add styles and some basic js presentation stuff.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,23 +1,107 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import {Socket} from "phoenix"
+import $ from 'jquery'
+import ace from 'brace'
+import 'brace/mode/javascript'
+import 'brace/theme/github'
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "deps/phoenix_html/web/static/js/phoenix_html"
+require('css/app')
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+window.$ = $
 
-// import socket from "./socket"
+const Editor = (() => {
+  var editor = null;
 
-$('.slide')
+  function create() {
+    editor = ace.edit('editor')
+    editor.getSession().setMode('ace/mode/javascript')
+    editor.setTheme('ace/theme/github')
+  }
+
+  function toggle() {
+    $('body').toggleClass('editor-open')
+  }
+
+  return {
+    new: create,
+    toggle: toggle
+  }
+})();
+
+const Router = (() => {
+  function navigate(path) {
+    var newPath = path ? path : ''
+      , root    = window.location.href.replace(/#(.*)$/, '')
+      , newRoute = `${root}#/${newPath}`
+
+    window.location.href = newRoute
+
+    return this
+  }
+
+  function path() {
+    return window.location.href.match(/#\/(.*)$/)[1]
+  }
+
+  return {
+    navigate: navigate,
+    path: path
+  }
+})();
+
+const Deck = (() => {
+  var $slides       = $('.slide')
+    , currentSlide = null
+
+  $(window).on('hashchange', _updateCurrentSlide)
+
+  _updateCurrentSlide()
+
+  function next() {
+    if ( currentSlide < $slides.length-1 )
+      Router.navigate(currentSlide + 1)
+  }
+
+  function previous() {
+    if ( currentSlide && currentSlide != 0 )
+      Router.navigate(currentSlide - 1)
+  }
+
+  function _updateCurrentSlide() {
+    currentSlide = parseInt( Router.path() ) || 0
+    _displaySlide(currentSlide)
+  }
+
+  function _displaySlide(id) {
+    $slides.removeClass('current').eq(id).addClass('current')
+  }
+
+  return {
+    next: next
+  , previous: previous
+  }
+})();
+
+Editor.new()
+
+$(document).on('keydown', keyHandler)
+
+function keyHandler(e) {
+  switch (e.which) {
+    case 39: //right
+      Deck.next()
+      break;
+    case 37: //left
+      Deck.previous()
+      break;
+    case 13: // enter
+      console.log('Enter')
+      break;
+    case 69: // e
+      Editor.toggle()
+      break;
+    default:
+      console.log('Unhandled');
+      console.log(e);
+      break;
+  }
+}
