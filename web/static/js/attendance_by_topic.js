7c4f82f1f21c19e5bde6c9997103967999f7dafc
Chris Keathley <c@keathley.io>
2015-11-06 17:21:37 -0500

    Sortable attendance by topic

--- web/static/js/attendance_by_topic.js
+++ web/static/js/attendance_by_topic.js
@@ -1,16 +1,16 @@
 import d3 from 'd3'
 import d3Tip from 'd3-tip'
+import $ from 'jquery'
 
-var margin = {top: 200, right: 20, bottom: 30, left: 40}
+var margin = {top: 100, right: 20, bottom: 30, left: 40}
   , width  = 1240 - margin.left - margin.right
-  , height = 600 - margin.top - margin.bottom
+  , height = 800 - margin.top - margin.bottom
   , x      = d3.scale.ordinal()
   , y      = d3.scale.linear()
   , color  = d3.scale.category10()
-  // , color  = d3.scale.category20()
-  // , color  = d3.scale.category20c()
   , xAxis  = d3.svg.axis().scale(x).orient('bottom').ticks(0)
   , yAxis  = d3.svg.axis().scale(y).orient('left')
+  , data   = null
 
 var tip = d3Tip()
   .attr('class', 'd3-tip')
@@ -24,7 +24,7 @@ var tip = d3Tip()
   })
 
 d3.json('/json/events.json', (error, json) => {
-  var data = munge(json)
+  data = munge(json)
 
   x.domain(data.map((d) => d.name)).rangeRoundBands([0, width], 0.1)
   y.domain([0, d3.max(data, (d) => d.attended)]).range([height, 0])
@@ -68,6 +68,19 @@ d3.json('/json/events.json', (error, json) => {
       .duration(300)
       .attr('y', (d) => y(d.attended))
       .attr('height', (d) => height - y(d.attended))
+
+  $('.js-sort-topics').on('click', (e) => {
+    var sorted = data.sort((a, b) => a.attended - b.attended).map((d) => d.name)
+      , x0 = x.domain(sorted).copy()
+      , transition = svg.transition().duration(500)
+      , delay = (d, i) => i * 5
+
+    svg.selectAll('rect').sort((a, b) => x0(a.name) - x0(b.name))
+
+    transition.selectAll('rect')
+      .delay(delay)
+      .attr('x', (d) => x0(d.name))
+  })
 });
 
 function munge(json) {
@@ -75,22 +88,4 @@ function munge(json) {
   json.forEach((d, i) => { d.x = i })
 
   return json
-}
-
-const TOPIC_COLORS = {
-  niche: '#585858'
-, backend: '#ab4642'
-, frontend: '#a1b56c'
-, devops: '#a16946'
-, discussion: '#dc9656'
-, technology: '#ba8baf'
-, theory: '#7cafc2'
-, lightning: '#fbec26'
-, technique: '#c028f5'
-, datascience: 'rgb(45, 46, 191)'
-}
-
-function color(t) {
-  console.log(t);
-  return TOPIC_COLORS[t]
 }
